6e2dd82e0025661d55e29bd761c3d722c708dc01
thahsinb <thahsin.begum.78676@gmail.com>
2021-09-21 21:13:03 +0530

    Exercise 5

---
+++ learn-python-hardway/Ex5.py
@@ -0,0 +1,16 @@
+my_name = "Thahsin Begum"
+my_age = 21
+my_height = 165
+my_weight = 68
+my_eyes = 'Black'
+my_teeth = 'White'
+my_hair = 'Black'
+
+print("Let's talk about",my_name)
+print("I'm", my_height, "cm tall.")
+print("I'm weigh %d Kgs." %my_weight)
+print("Actually that's not too heavy.")
+print("I have got %s eyes and %s hair." %(my_eyes,my_hair))
+print("My teeth are usually %s depending on the chocolate I eat." %my_teeth)
+
+print("If I add %d, %d and %d. I get %d." %(my_age,my_height,my_weight, my_age+my_height+my_weight))
