5032403ec8025ab86f00669d6647c3dc7f686624
thahsinb <thahsin.begum.78676@gmail.com>
2021-09-24 18:59:34 +0530

    Exercise 5 study drill 1

--- learn-python-hardway/ex5/st1.py
+++ learn-python-hardway/ex5/st1.py
@@ -8,10 +8,12 @@ hair = 'Black'
 
 print("Let's talk about", name)
 print("I'm", height, "cm tall.")
-print("I'm weigh %d Kgs." % weight)
+print("I weigh %d Kgs." % weight)
 print("Actually that's not too heavy.")
 print("I have got %s eyes and %s hair." % (eyes, hair))
 print("My teeth are usually %s depending on the chocolate I eat." % teeth)
 
 print("If I add %d, %d and %d. I get %d." % (age, height, weight, age + height + weight))
+
+round(1.7333)
 
